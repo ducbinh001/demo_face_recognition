17c5c522bfb5870b91a722be609b9626b4f7ffdd
BinhKieu <kieubinh96@gmail.com>
2020-03-26 14:33:25 +0700

    crop canvas

--- src/public/scripts/displayCamera.js
+++ src/public/scripts/displayCamera.js
@@ -4,7 +4,7 @@ const controls = document.querySelector('.controls');
 const cameraOptions = document.querySelector('.video-options>select');
 const video = document.querySelector('video');
 const canvas = document.querySelector('canvas');
-// const screenshotImage = document.querySelector('img');
+const screenshotImage = document.querySelector('img');
 const buttons = [...controls.querySelectorAll('button')];
 var interval = null;
 let streamStarted = false;
@@ -86,7 +86,7 @@ const detectFaceOnCamera = () => {
     result.forEach(ele => {
       console.log(ele)
       ctx.beginPath();
-      ctx.lineWidth = "3";
+      ctx.lineWidth = "1";
       ctx.strokeStyle = "red";
       ctx.rect(ele.box.x, ele.box.y, ele.box.width, ele.box.height);
       ctx.stroke();
@@ -94,20 +94,39 @@ const detectFaceOnCamera = () => {
   }, 20)
 };
 
+const cropFace = async () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  const result = await faceapi.detectAllFaces(video);
+  let ele = result[0]
+
+  var tempCanvas = document.createElement("canvas");
+  var tCtx = tempCanvas.getContext("2d");
+
+  screenshotImage.setAttribute("width", ele.box.width);
+  screenshotImage.setAttribute("height", ele.box.height);
+
+  tempCanvas.width = ele.box.width;
+  tempCanvas.height = ele.box.height;
+
+  tCtx.drawImage(canvas, ele.box.x, ele.box.y, ele.box.width, ele.box.height, 0, 0, ele.box.width, ele.box.height);
+
+  screenshotImage.src = tempCanvas.toDataURL('image/webp');
+}
+
 pause.onclick = pauseStream;
-screenshot.onclick = detectFaceOnCamera;
+screenshot.onclick = cropFace;
 
 const startStream = async (constraints) => {
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
   handleStream(stream);
 };
 
-
 const handleStream = (stream) => {
   video.srcObject = stream;
   play.classList.add('d-none');
   pause.classList.remove('d-none');
-  // screenshot.classList.remove('d-none');
+  screenshot.classList.remove('d-none');
 };
 
 
